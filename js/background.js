9a3a5f5467cf095d45973bd2998187410d547e51
0410Wzn <Wzn17854862639@gmail.com>
2021-12-04 22:35:31 +0800

    Site updated: 2021-12-04 22:35:30

--- js/background.js
+++ js/background.js
@@ -1,8 +1,9 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-    "url()",
-    "url()",
-    "url(https://gitee.com/youthlql/randombg/raw/master/bg/00184.webp)"
+    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/im02l空(夕).jpg)",
+    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/bg04l三咲町08繁華街-(昼).bmp)",
+    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/im09l04旧校舎へ続く森.bmp)",
+    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/hexobackground/im01オープニング12_背景d2.jpg)"
 ];
 //获取背景图片总数，生成随机数
 var bgindex =Math.ceil(Math.random() * (backimg.length-1));
