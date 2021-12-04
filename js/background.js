022fe8138daea4e411b802d377cd09c5f855e746
0410Wzn <Wzn17854862639@gmail.com>
2021-12-04 22:53:43 +0800

    Site updated: 2021-12-04 22:53:42

--- js/background.js
+++ js/background.js
@@ -2,7 +2,6 @@
 var backimg =[
     "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/im02l空(夕).jpg)",
     "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/bg04l三咲町08繁華街-(昼).bmp)",
-    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/im09l04旧校舎へ続く森.bmp)",
     "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/hexobackground/im01オープニング12_背景d2.jpg)"
 ];
 //获取背景图片总数，生成随机数
