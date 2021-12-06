0b980176892afe3789d8b868476a8a8b2ab67f64
0410Wzn <Wzn17854862639@gmail.com>
2021-12-07 00:33:12 +0800

    Site updated: 2021-12-07 00:33:11

--- js/background.js
+++ js/background.js
@@ -1,15 +1,13 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/im02l空(夕).jpg)",
-    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/bg04l三咲町08繁華街-(昼).bmp)",
-    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/im09l04旧校舎へ続く森.bmp)",
-    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/bg04l三咲町08繁華街-(夜).bmp)",
-    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/im17ep01(背景).bmp)",
-    "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/hexobackground/im01オープニング12_背景d2.jpg)"
+  "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/im02l空(夕).jpg)",
+  "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/bg04l三咲町08繁華街-(昼).bmp)",
+  "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/im09l04旧校舎へ続く森.bmp)",
+  "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/bg04l三咲町08繁華街-(夜).bmp)",
+  "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/CTF/im17ep01(背景).bmp)",
+  "url(https://wzn0410.oss-cn-beijing.aliyuncs.com/img/hexobackground/im01オープニング12_背景d2.jpg)"
 ];
 //获取背景图片总数，生成随机数
-var bgindex =Math.ceil(Math.random() * (backimg.length-1));
+var bgindex = Math.floor(Math.random() * (backimg.length));
 //重设背景图片
 document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
-//重设banner图片
-document.getElementById("page-header").style.backgroundImage = backimg[bgindex];
